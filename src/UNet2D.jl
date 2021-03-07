c0c1703e471279ccc8ca5db94df98744c5bed462
urlicht <jungsu93@gmail.com>
2021-03-07 11:14:41 -0500

    add util.jl; modify denepdency

--- src/UNet2D.jl
+++ src/UNet2D.jl
@@ -1,5 +1,14 @@
 module UNet2D
 
-using PyCall, Statistics, CUDA
+using PyCall, Statistics
 
+include("init.jl")
+include("model.jl")
+include("util.jl")
+
+export create_model,
+    eval_model,
+    # util.jl
+    standardize,
+    reshape_array
 end
--- src/model.jl
+++ src/model.jl
@@ -1,9 +1,26 @@
-function create_model(path_model, n_ch_input, n_class, n_feature_init; device=torch_device)
+function create_model(n_ch_input, n_class, n_feature_init,path_model=nothing; device=torch_device, eval_mode=true)
     model = py_unet2d.model.unet_model.UNet2D(n_ch_input, n_class, n_feature_init, false)
     
     model.to(device)
-    model.load_state_dict(torch.load(path_model, map_location=device))
-    model.eval()
+    !isnothing(path_model) && model.load_state_dict(torch.load(path_model, map_location=device))
+    eval_mode && model.eval()
     
     return model
+end
+
+function eval_model(img::Union{Array{Float32,2}, Array{Float32,3}}, model; device=torch_device)
+    img_x = reshape_array(img)
+    img_x = py_torch.from_numpy(img_x).to(device)
+    
+    @pywith py_torch.no_grad() begin
+        img_y_pred = model(img_x)
+        img_y_pred = py_torch.nn.functional.sigmoid(img_y_pred)
+        img_y_pred = img_y_pred.cpu().numpy()
+        
+        if size(img_y_pred)[1] == 1
+            return img_y_pred[1,1,:,:]
+        else
+            return img_y_pred[:,1,:,:]
+        end
+    end
 end
